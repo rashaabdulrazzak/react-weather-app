7f7e0595fce43322385684b96e0de2f91288ce2c
rashaabdulrazzak <rasha.abdulrazzak@gmail.com>
2023-04-06 17:43:29 +0300

    change the url to https

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -8,7 +8,7 @@ function SearchMain() {
 
   const getWeatherInfo = async () => {
     try {
-      let url = `http://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&units=metric&appid=f840f08444c59ce09a2fbb87f9bff214`
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&units=metric&appid=f840f08444c59ce09a2fbb87f9bff214`
 
       let res = await fetch(url)
       let data = await res.json()
